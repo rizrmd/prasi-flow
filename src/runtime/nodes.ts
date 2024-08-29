b3ebb7ba6f125539e2be1294d5c75787d326373a
Rizky <rizky@avolut.com>
2024-08-29 15:06:08 +0700

    adding code sample

--- src/runtime/nodes.ts
+++ src/runtime/nodes.ts
@@ -1,4 +1,5 @@
+import { codeNode } from "./node_types/code";
 import { startNode } from "./node_types/start";
 
-export const allNodeDefinitions = { start: startNode };
+export const allNodeDefinitions = { start: startNode, code: codeNode };
 export type PRASI_NODE_DEFS = typeof allNodeDefinitions;
--- src/runtime/test/fixture.ts
+++ src/runtime/test/fixture.ts
@@ -3,9 +3,17 @@ import { PF } from "../types";
 
 export const sampleFlow = () => {
   const start = createNode({ type: "start" });
+  const code = createNode({
+    type: "code",
+    code: `\
+() => {
+  console.log('ini code');
+}`,
+  });
+
   const result = {
     name: "test",
-    nodes: [start],
+    nodes: [start, code],
   };
   return result as PF;
 };
