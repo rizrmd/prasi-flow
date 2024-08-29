2f3f702105ced1f36b99bb2aa36ded31e4a40e1c
Rizky <rizky@avolut.com>
2024-08-29 14:24:46 +0700

    fix

--- src/runtime/lib/create-node.ts
+++ src/runtime/lib/create-node.ts
@@ -4,10 +4,8 @@ import { allNodeDefinitions, PRASI_NODE_DEFS } from "../nodes";
 export const createNode = <
   T extends keyof PRASI_NODE_DEFS,
   K extends {
-    [J in keyof Exclude<PRASI_NODE_DEFS[T]["input"], undefined>]: Exclude<
-      PRASI_NODE_DEFS[T]["input"],
-      undefined
-    >[J]["default"];
+    //@ts-ignore
+    [J in keyof PRASI_NODE_DEFS[T]["input"]]: PRASI_NODE_DEFS[T]["input"][J]["default"];
   }
 >(
   type: T,
--- src/runtime/test/fixture.ts
+++ src/runtime/test/fixture.ts
@@ -2,7 +2,7 @@ import { createNode } from "../lib/create-node";
 import { PF } from "../types";
 
 export const sampleFlow = () => {
-  const start = createNode("start", { default: { coba: "haloa" } });
+  const start = createNode("start", { default: { coba: "a" } });
   const result = {
     name: "test",
     nodes: [start],
