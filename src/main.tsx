bd36401b50be60005710ea003e9914efd71626ce
Rizky <rizky@avolut.com>
2024-09-02 12:02:51 +0700

    add pane

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import { PrasiFlow } from "./flow/prasi-flow";
 
 export const Main = () => {
   return (
     <div className={cx("fixed inset-0 w-screen h-screen")}>
-      <PrasiFlow />
+      <PanelGroup direction="vertical">
+        <Panel>
+          <PrasiFlow />
+        </Panel>
+        <PanelResizeHandle className={"border-t"} />
+        <Panel defaultSize={25}></Panel>
+      </PanelGroup>
+      ;
     </div>
   );
 };
