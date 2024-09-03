5376714cf77b3daec9203eb807564852890eec9e
Rizky <rizky@avolut.com>
2024-09-03 13:07:09 +0700

    adding flow

--- src/flow/prop/prasi-flow-fields.tsx
+++ src/flow/prop/prasi-flow-fields.tsx
@@ -6,10 +6,14 @@ export const PrasiFlowFields: FC<{ node: PFNode }> = ({ node }) => {
   const field = getNodeFields(node);
   return (
     <>
-      {Object.values(field?.definition?.fields || {})
-        .sort((a, b) => a.idx - b.idx)
-        .map((field) => {
-          return <div key={field.idx}>{field.label}</div>;
+      {Object.entries(field?.definition?.fields || {})
+        .sort((a, b) => a[1].idx - b[1].idx)
+        .map(([key, field]) => {
+          return (
+            <div key={field.idx}>
+              {field.label} {node[key]}
+            </div>
+          );
         })}
     </>
   );
